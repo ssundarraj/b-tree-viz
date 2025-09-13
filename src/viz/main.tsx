00a81377e4f871cee45b9bd2524bd5581251bebf
Sriram Sundarraj <ssundarraj@gmail.com>
2025-09-12 22:37:30 -0700

    gh pages routing

--- src/viz/main.tsx
+++ src/viz/main.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+// Handle GitHub Pages 404 redirect
+const redirect = sessionStorage.redirect;
+delete sessionStorage.redirect;
+if (redirect && redirect !== location.href) {
+  history.replaceState(null, null as any, redirect);
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
